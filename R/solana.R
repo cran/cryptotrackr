6f40d15da5f7408f2d53798fee2bb77bad4d2732
Trevor French <FrenchTrevor@outlook.com>
2024-06-09 06:20:05 +0000

    version 1.3.3

--- R/solana.R
+++ R/solana.R
@@ -2,28 +2,40 @@
 #'
 #' @param url the RPC url for your API call
 #' @param request_body the request body for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns data from your Solana API call
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
 #' request_body <-
 #'   solana_assemble_request_body('"2.0"', 'null', '"getBlockHeight"', NULL)
-#' data <- solana_api_call(url, request_body)
+#' data <- solana_api_call(url, request_body)}
 
-solana_api_call <- function(url, request_body) {
+solana_api_call <- function(url, request_body, timeout_seconds = 60) {
   headers <- c("Content-Type" = "application/json")
-  response <-
-    httr::POST(url, httr::add_headers(.headers = headers), body = request_body)
 
-  if (httr::status_code(response) == 200) {
-    return(httr::content(response))
-  } else {
-    print(httr::content(response, as = "text"))
-    stop("Request failed with status code: ",
-         httr::status_code(response))
-  }
+  tryCatch({
+    res <-
+      httr::POST(url
+                 , httr::add_headers(.headers = headers)
+                 , body = request_body
+                 , httr::timeout(timeout_seconds))
+
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' solana_assemble_key_pair
@@ -76,12 +88,13 @@ solana_assemble_list <- function(character_vector) {
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' limit <- solana_assemble_key_pair('limit', NULL)
 #' character_vector <- c(limit)
 #' config_object <- solana_assemble_list(character_vector)
 #' address <- "Vote111111111111111111111111111111111111111"
 #' params <- paste('["', address, '", {', config_object, '}]', sep = '')
-#' solana_assemble_request_body('"2.0"', 'null', '"getSignaturesForAddress"', params)
+#' solana_assemble_request_body('"2.0"', 'null', '"getSignaturesForAddress"', params)}
 
 solana_assemble_request_body <-
   function(jsonrpc, id, method, params) {
@@ -101,6 +114,7 @@ solana_assemble_request_body <-
 #' @param address the address for which you're retrieving signatures
 #' @param limit maximum transaction signatures to return (between 1 and 1,000).
 #' Default is 1,000.
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns signatures for confirmed transactions that include the given
 #' address in their accountKeys list. Returns signatures backwards in time from
@@ -108,222 +122,375 @@ solana_assemble_request_body <-
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
 #' address <- "Vote111111111111111111111111111111111111111"
-#' data <- solana_get_signature_for_address(url,address)
+#' data <- solana_get_signature_for_address(url,address)}
 
 solana_get_signature_for_address <-
-  function(url, address, limit = NULL) {
+  function(url, address, limit = NULL, timeout_seconds = 60) {
     limit <- solana_assemble_key_pair('limit', limit)
     character_vector <- c(limit)
     config_object <- solana_assemble_list(character_vector)
     params <- paste('["', address, '", {', config_object, '}]', sep = '')
     request_body <-
       solana_assemble_request_body('"2.0"', 'null', '"getSignaturesForAddress"', params)
-    data <- solana_api_call(url, request_body)
-    return(data$result)
+    data <- solana_api_call(url, request_body, timeout_seconds)
+
+    if (is.null(data)) {
+      warning("Failed to retrieve data from Solana API.")
+      return(NULL)
+    }
+
+    if (!is.null(data$result)) {
+      return(data$result)
+    } else {
+      warning("The response does not contain 'result'.")
+      return(NULL)
+    }
+
   }
 
 #' solana_get_account_info
 #'
 #' @param url the RPC url for your API call
 #' @param pubkey the pubkey for which you're retrieving account info
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns all information associated with the account of provided Pubkey
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
 #' pubkey <- "vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg"
-#' data <- solana_get_account_info(url, pubkey)
+#' data <- solana_get_account_info(url, pubkey)}
 
-solana_get_account_info <- function(url, pubkey) {
+solana_get_account_info <- function(url, pubkey, timeout_seconds = 60) {
   params <- paste('["', pubkey, '"]', sep = '')
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getAccountInfo"', params)
-  data <- solana_api_call(url, request_body)
-  return(data$result$value)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result$value)) {
+    return(data$result$value)
+  } else {
+    warning("The response does not contain 'result$value'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_block_height
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the current block height of the node
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_block_height(url)
+#' data <- solana_get_block_height(url)}
 
-solana_get_block_height <- function(url) {
+solana_get_block_height <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getBlockHeight"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_health
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the current health of the node.
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_health(url)
+#' data <- solana_get_health(url)}
 
-solana_get_health <- function(url) {
+solana_get_health <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getHealth"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
 }
 
 #' solana_get_version
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the current Solana version running on the node
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_version(url)
+#' data <- solana_get_version(url)}
 
-solana_get_version <- function(url) {
+solana_get_version <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getVersion"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_supply
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns information about the current supply.
 #' @export
 #'
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
 #' data <- solana_get_supply(url)}
 
-solana_get_supply <- function(url) {
+solana_get_supply <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getSupply"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result$value)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result$value)) {
+    return(data$result$value)
+  } else {
+    warning("The response does not contain 'result$value'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_identity
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the identity pubkey for the current node
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_identity(url)
+#' data <- solana_get_identity(url)}
 
-solana_get_identity <- function(url) {
+solana_get_identity <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getIdentity"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result$identity)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result$identity)) {
+    return(data$result$identity)
+  } else {
+    warning("The response does not contain 'result$identity'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_inflation_rate
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the specific inflation values for the current epoch
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_inflation_rate(url)
+#' data <- solana_get_inflation_rate(url)}
 
-solana_get_inflation_rate <- function(url) {
+solana_get_inflation_rate <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getInflationRate"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_genesis_hash
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the genesis hash
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_genesis_hash(url)
+#' data <- solana_get_genesis_hash(url)}
 
-solana_get_genesis_hash <- function(url) {
+solana_get_genesis_hash <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', 'null', '"getGenesisHash"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_recent_prioritization_fees
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns a list of prioritization fees from recent blocks.
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_recent_prioritization_fees(url)
+#' data <- solana_get_recent_prioritization_fees(url)}
 
-solana_get_recent_prioritization_fees <- function(url) {
+solana_get_recent_prioritization_fees <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', '1', '"getRecentPrioritizationFees"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_slot
 #'
 #' @param url the RPC url for your API call
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns the slot that has reached the given or default commitment
 #' level.
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
-#' data <- solana_get_slot(url)
+#' data <- solana_get_slot(url)}
 
-solana_get_slot <- function(url) {
+solana_get_slot <- function(url, timeout_seconds = 60) {
   request_body <-
     solana_assemble_request_body('"2.0"', '1', '"getSlot"', NULL)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
 
 #' solana_get_block
 #'
 #' @param url the RPC url for your API call
 #' @param slot slot number, as u64 integer
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return Returns identity and transaction information about a confirmed block
 #' in the ledger.
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' url <- "https://api.devnet.solana.com"
 #' slot <- solana_get_slot(url)
-#' data <- solana_get_block(url, slot)
+#' data <- solana_get_block(url, slot)}
 
-solana_get_block <- function(url, slot) {
+solana_get_block <- function(url, slot, timeout_seconds = 60) {
   params <- paste('[', slot, ',
       {
         "encoding": "json",
@@ -333,6 +500,18 @@ solana_get_block <- function(url, slot) {
       }]', sep = '')
   request_body <-
     solana_assemble_request_body('"2.0"', '1', '"getBlock"', params)
-  data <- solana_api_call(url, request_body)
-  return(data$result)
+  data <- solana_api_call(url, request_body, timeout_seconds)
+
+  if (is.null(data)) {
+    warning("Failed to retrieve data from Solana API.")
+    return(NULL)
+  }
+
+  if (!is.null(data$result)) {
+    return(data$result)
+  } else {
+    warning("The response does not contain 'result'.")
+    return(NULL)
+  }
+
 }
--- tests/testthat/test-solana.R
+++ tests/testthat/test-solana.R
@@ -1,70 +1,173 @@
 test_that('the "solana_get_signature_for_address" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
   address <- "Vote111111111111111111111111111111111111111"
-  data <- solana_get_signature_for_address(url, address)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_signature_for_address(url, address),
+    error = function(c) geterrmessage()
+    )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_account_info" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
   pubkey <- "vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg"
-  data <- solana_get_account_info(url, pubkey)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_account_info(url, pubkey),
+    error = function(c) geterrmessage()
+    )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_account_info" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_block_height(url)
-  expect_type(data, 'integer')
+  data <- tryCatch(
+    solana_get_block_height(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'integer')
+  }
 })
 
 test_that('the "solana_get_health" function returns a character', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_health(url)
-  expect_type(data, 'character')
+  data <- tryCatch(
+    solana_get_health(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'character')
+  }
 })
 
 test_that('the "solana_get_version" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_version(url)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_version(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_supply" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_supply(url)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_supply(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_identity" function returns a character', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_identity(url)
-  expect_type(data, 'character')
+  data <- tryCatch(
+    solana_get_identity(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'character')
+  }
 })
 
 test_that('the "solana_get_inflation_rate" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_inflation_rate(url)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_inflation_rate(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_genesis_hash" function returns a character', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_genesis_hash(url)
-  expect_type(data, 'character')
+  data <- tryCatch(
+    solana_get_genesis_hash(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'character')
+  }
 })
 
 test_that('the "solana_get_slot" function returns an integer and the
           "solana_get_block" function returns a list', {
+            skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  slot <- solana_get_slot(url)
-  data <- solana_get_block(url, slot)
-  expect_type(slot, 'integer')
-  expect_type(data, 'list')
+  slot <- tryCatch(
+    solana_get_slot(url),
+    error = function(c) geterrmessage()
+  )
+  if (slot[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  data <- tryCatch(
+    solana_get_block(url, slot),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(slot, 'integer')
+    expect_type(data, 'list')
+  }
 })
 
 test_that('the "solana_get_recent_prioritization_fees" function returns a list', {
+  skip_on_cran()
   url <- "https://api.devnet.solana.com"
-  data <- solana_get_recent_prioritization_fees(url)
-  expect_type(data, 'list')
+  data <- tryCatch(
+    solana_get_recent_prioritization_fees(url),
+    error = function(c) geterrmessage()
+  )
+  # if (data[1] == "Request failed with status code: 429") {skip("Rate Limit")}
+  if (is.null(data)) {
+    skip('warning likely due to rate limit')
+  } else {
+    expect_type(data, 'list')
+  }
 })
